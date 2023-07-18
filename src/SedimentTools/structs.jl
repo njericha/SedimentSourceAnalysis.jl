793707e123fe31854d282a0ef53e2c8098221497
Nicholas Richardson <njericha@uwaterloo.ca>
2023-07-18 11:45:34 -0700

    grain interfaces

--- src/SedimentTools/structs.jl
+++ src/SedimentTools/structs.jl
@@ -30,7 +30,24 @@ to_tensor(D::Distributions) = cat(D.data..., dims=3)
 ############
 
 struct Grain{T} <: AbstractVector{T}
-    #Eu::T
-    #Ti::T
-    #...
+    ordereddict::OrderedDict{String, T}
 end
+
+# Interface
+# allow for alternate backend like NamedTuple, NamedArray, etc.
+OrderedDict{String,T}(g::Grain{T}) where T <: Number = g.ordereddict
+
+import Base: size, getindex, keys, values, collect, size, getindex, iterate, convert
+Base.Vector{T}(g::Grain{T}) where T <: Number = values(g)
+
+"""List of names for each measurement"""
+Base.keys(g::Grain) = keys(OrderedDict(g))
+
+"""A plain Vector{T} with the values for each measurment"""
+Base.values(g::Grain) = values(OrderedDict(g))
+
+Base.collect(g::Grain) = collect(OrderedDict(g))
+Base.size(g::Grain) =(length(Vector(g)),)
+
+"""gets the i'th (name, value) pair in the grain"""
+Base.getindex(g::Grain, key::String) = OrderedDict(g)[key]
