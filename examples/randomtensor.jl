6894424994670e03ec7131649f56caac9aa887a5
Nicholas Richardson <njericha@uwaterloo.ca>
2023-07-27 12:46:23 -0700

    clean up printing, required Printf

--- examples/randomtensor.jl
+++ examples/randomtensor.jl
@@ -6,33 +6,39 @@ using Random
 using Statistics: mean
 using SedimentAnalysis
 using Plots
+using Printf
 
 # set random seed for repeatability
-Random.seed!(314159)
+Random.seed!(314159265)
 
+# make random matrix and tensors C[i,r], F[r,j,k] for some R
 M, N, P = (50, 50, 50)
 R = 5
-# make random matrix and tensors C[i,r], F[r,j,k] for some R
 C_true = abs.(randn((M, R)))
 F_true = abs.(randn((R, N, P)))
 
-Y = C_true * F_true # set Y[i,j,k] = C[i,r] * F[r,j,k]
+# Normalize C. This gives us some scaling we can use for uniqueness
+C_true ./= mean(C_true)
+
+Y = C_true * F_true # set Y[i,j,k] = \sum_r C[i,r] * F[r,j,k]
+# size(Y) == (M, N, P)
 
 # Perform the nonnegative decomposition Y=CF
 rank = 5
-C, F, rel_errors, norm_grad, dist_Ncone = nnmtf(Y, rank, tol=2, maxiter=1500; rescale_Y=false);
+C, F, rel_errors, norm_grad, dist_Ncone = nnmtf(Y, rank, tol=1, maxiter=1500; rescale_Y=false);
 
 # Plot Convergence
 plots = plot_convergence(rel_errors, norm_grad, dist_Ncone)
 display.(plots)
 
-# Normalize C. This gives us a consistant scaling for C and F, and so
-# C should match the true C. The relative sizes between entries do not change
-mean_C = mean(C) / mean(C_true)
+# Normalize C. To match our known scaling. The relative sizes between entries do not change
+mean_C = mean(C)
 C ./= mean_C
 F .*= mean_C
 
-match_sources!(C, F, C_true, F_true) # Because the order of sources could be different
+# Because the order of columns of C/ horizontal slices of F could be different,
+# we permute them to match the orginial ordering. Now the solution (should be?) unique
+match_sources!(C, F, C_true, F_true)
 
 # Show the relative error between the learned and true matrix/tensor
 # TODO use other metrics like RMSE and SNR
@@ -41,17 +47,18 @@ match_sources!(C, F, C_true, F_true) # Because the order of sources could be dif
 @show rel_error(C * F, Y) # < 1% error which is good!
 
 # Repeat for rank in {1, 2, ..., 10}
-# Note rank = 5 is the smallest rank where the relative error plateaus
 ranks = 1:10
 final_rel_error = zeros(length(ranks))
 final_norm_grad = zeros(length(ranks))
 final_dist_Ncone = zeros(length(ranks))
+println("rank | n_iterations | relative error")
 for rank in ranks
-    C, F, rel_errors, norm_grad, dist_Ncone = nnmtf(Y, rank, tol=2, maxiter=1500; rescale_Y=false);
+    C, F, rel_errors, norm_grad, dist_Ncone = nnmtf(Y, rank, tol=1, maxiter=1500; rescale_Y=false);
     final_rel_error[rank] = rel_errors[end]
     final_norm_grad[rank] = norm_grad[end]
     final_dist_Ncone[rank] = dist_Ncone[end]
-    @show final_rel_error[rank], final_norm_grad[rank], final_dist_Ncone[rank]
+    @printf("%4i | %12i | %3.3g\n",
+        rank, length(rel_errors), final_rel_error[rank])
 end
 
 # Visualizing convergence
